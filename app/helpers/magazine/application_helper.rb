d2b0a907964c3fd05364771530b7ed87b781b175
kozo yamagata <tune002@gmail.com>
2015-02-04 10:56:35 +0900

    Implement fallback to main_app helper

--- app/helpers/magazine/application_helper.rb
+++ app/helpers/magazine/application_helper.rb
@@ -18,6 +18,30 @@ module Magazine
       ['magazine', 'articles', 'shared', category.try(:category_slug), article.try(:slug), name].compact.join('/')
     end
 
+    def method_missing method, *args, &block
+      if method.to_s.end_with?('_path') or method.to_s.end_with?('_url')
+        if main_app.respond_to?(method)
+          main_app.send(method, *args)
+        else
+          super
+        end
+      else
+        super
+      end
+    end
+
+    def respond_to?(method)
+      if method.to_s.end_with?('_path') or method.to_s.end_with?('_url')
+        if main_app.respond_to?(method)
+          true
+        else
+          super
+        end
+      else
+        super
+      end
+    end
+
     private
       def _magazine_val(name, options)
         val = nil
